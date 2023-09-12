7cbe7d2e92aab224b3e4b864dc300bc6469f8295
Reis567 <matheusdosreis9@gmail.com>
2023-09-12 06:49:36 -0300

    urls and views of user auth

--- ChatProject/urls.py
+++ ChatProject/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from ..chat import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', views.custom_logout, name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+
+
 
 # Create your views here.
+
+
+# View de login personalizada
+class CustomLoginView(LoginView):
+    template_name = 'user/login.html'  
+
+
+def custom_logout(request):
+    # Realiza o logout do usuário
+    LogoutView.as_view()(request) 
+
+    return render(request, 'user/logout.html')  
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Conta criada para {username}! Agora você pode fazer login.')
+            return redirect('login')  
+    else:
+        form = UserCreationForm()
+    return render(request, 'user/register.html', {'form': form})
