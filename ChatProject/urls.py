ebd3b2eadbb10e38934ec29bfa48f6b3387b0be6
Reis567 <matheusdosreis9@gmail.com>
2023-09-12 08:55:11 -0300

    urls division

--- ChatProject/urls.py
+++ ChatProject/urls.py
@@ -15,14 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.contrib.auth import views as auth_views
 from chat import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' , views.home , name='home'),
+
+    ## User urls
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.custom_logout, name='logout'),
     path('register/', views.register, name='register'),
+
+
+    ## Chat urls
+    path('chat/', include('chat.urls')),
+    path('' , views.home , name='home'),
 ]
