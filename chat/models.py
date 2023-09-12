f88994d9994150b315f37a60ad18553fc85bf776
Reis567 <matheusdosreis9@gmail.com>
2023-09-12 18:13:41 -0300

    models and admin site register

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Room(models.Model):
+    name = models.CharField(max_length=20)
+    slug = models.SlugField(max_length=50)
+
+class Message(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
