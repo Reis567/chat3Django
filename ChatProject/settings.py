bd6dbba52cc71271a0edab05fff843a5dcc2d979
Reis567 <matheusdosreis9@gmail.com>
2023-09-12 08:00:40 -0300

    logout redirect url

--- ChatProject/settings.py
+++ ChatProject/settings.py
@@ -119,8 +119,8 @@ USE_I18N = True
 USE_TZ = True
 
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/login/'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'login'
 
 
 # Static files (CSS, JavaScript, Images)
