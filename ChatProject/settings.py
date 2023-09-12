0ef51ae99ec93393295654ddfac1596158a31a58
Reis567 <matheusdosreis9@gmail.com>
2023-09-12 07:30:47 -0300

    css settings

--- ChatProject/settings.py
+++ ChatProject/settings.py
@@ -126,7 +126,11 @@ LOGOUT_REDIRECT_URL = '/login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# Configuração da pasta static
+STATIC_URL = '/static/'
+
+# Caminho para a pasta static na raiz do projeto
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
