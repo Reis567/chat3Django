87b38c60610d617690eb3c05834296a5b9c9c2e9
Reis567 <matheusdosreis9@gmail.com>
2023-09-13 06:34:29 -0300

    verbose name and __str__

--- chat/models.py
+++ chat/models.py
@@ -1,14 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 class Room(models.Model):
+    class Meta:
+        verbose_name = 'Sala'
+        verbose_name_plural = 'Salas'
+
     name = models.CharField(max_length=20)
     slug = models.SlugField(max_length=50)
 
+    def __str__(self):
+        return self.name  # Ou qualquer outra representação que você desejar
+
 class Message(models.Model):
+
+    class Meta:
+        verbose_name = 'Mensagem'
+        verbose_name_plural = 'Mensagens'
+        
     content = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username}: {self.content}'  # Retorna o nome do usuário e o conteúdo da mensagem
