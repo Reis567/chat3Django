e0dbdf627b427ee3d6aa7c6a4c15e71464c4fe21
Reis567 <matheusdosreis9@gmail.com>
2023-09-22 07:06:40 -0300

    admin.site.register(UserProfile)

--- chat/admin.py
+++ chat/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Room,Message
+from .models import Room,Message,UserProfile
 # Register your models here.
 
 
 admin.site.register(Room)
 admin.site.register(Message)
+admin.site.register(UserProfile)
