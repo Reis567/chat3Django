388a2f4c3b2bfafaa96ab2b2a69db355b4a79944
Reis567 <matheusdosreis9@gmail.com>
2023-09-22 07:06:04 -0300

    user profile model

--- chat/models.py
+++ chat/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+def user_directory_path(instance, filename):
+    # Gera o caminho dinâmico baseado no nome de usuário e nome do arquivo
+    return f'profile_photos/{instance.user.username}/{filename}'
+
 class Room(models.Model):
     class Meta:
         verbose_name = 'Sala'
@@ -29,3 +33,15 @@ class Message(models.Model):
 
     def __str__(self):
         return f'{self.user.username}: {self.content}'  # Retorna o nome do usuário e o conteúdo da mensagem
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    photo = models.ImageField(upload_to=user_directory_path, blank=True, null=True)
+    description = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
