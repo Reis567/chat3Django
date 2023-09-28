a46f14558257d3401f351700b4cd1bc381276acf
Reis567 <matheusdosreis9@gmail.com>
2023-09-28 07:49:24 -0300

    sqlite to postgresql

--- ChatProject/settings.py
+++ ChatProject/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'ChatProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chat3Django_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Matheus123',
+        'HOST': 'localhost',  
+        'PORT': '5432',          
     }
 }
 
